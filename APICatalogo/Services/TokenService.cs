7ab4d02822f5e26b7d346a91b9b14b5df2db50e7
carlosedu11 <miguelodoom22@hotmail.com>
2025-05-05 08:46:39 -0300

    Implementação da autenticação JWT, Criação do serviço, Configuração do Indentity, Geração do token e do Refresh Token

--- APICatalogo/Services/TokenService.cs
+++ APICatalogo/Services/TokenService.cs
@@ -2,6 +2,7 @@
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace APICatalogo.Services
@@ -37,12 +38,41 @@ namespace APICatalogo.Services
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
-        }
+            var scureRandomBytes = new byte[128];
+
+            using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+            randomNumberGenerator.GetBytes(scureRandomBytes);
+            
+            var refreshToken = Convert.ToBase64String(scureRandomBytes);
+
+            return refreshToken
+       ; }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
         {
-            throw new NotImplementedException();
+            var secretKey = _config["JWT:SecretKey"] ?? throw new InvalidOperationException("Invalid Key!");
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token,tokenValidationParameters, out SecurityToken securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || 
+                !jwtSecurityToken.Header.Alg.Equals(
+                    SecurityAlgorithms.HmacSha256, 
+                    StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenArgumentException("Invalid Token!");
+            }
+            return principal;
         }
     }
 }
